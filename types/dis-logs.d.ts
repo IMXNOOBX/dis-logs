3533999319b91df4da20bbc2883218b0d34879be
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2024-02-04 17:38:28 +0100

    Update package version and dependencies & included ts types

---
+++ types/dis-logs.d.ts
@@ -0,0 +1,18 @@
+declare module 'dis-logs' {
+	class Webhook {
+	  constructor(webhook_url: string, name: string, avatar: string, console?: boolean);
+  
+	  set_name(name?: string): Promise<void>;
+	  set_avatar(base64Avatar?: string | null): Promise<void>;
+	  delete(): Promise<void>;
+	  get_time(): string;
+	  console(content: string): Promise<void>;
+	  success(content: string): Promise<void>;
+	  warn(content: string): Promise<void>;
+	  error(content: string): Promise<void>;
+	  send(content: string): Promise<void>;
+	}
+  
+	export { Webhook };
+  }
+  
