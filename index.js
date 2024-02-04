0afba37081897ce4af11621f7f7cd84fc17afe31
IMXNOOBX <paraelmobil403@gmail.com>
2024-02-04 16:32:14 +0100

    Update license information in index.js

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
  * @project dis-logs
  * @author IMXNOOBX
  *
- * @copyright (c) 2023 IMXNOOBX <https://github.com/IMXNOOBX>
+ * @copyright (c) 2024 IMXNOOBX <https://github.com/IMXNOOBX>
  *
  * @description
  * Simple nodejs library to be able to automaticall log /
  * your applications directly to your discord
  *
  * @license
- * Apache License 2.0
- * See <https://www.apache.org/licenses/LICENSE-2.0> for the full license text.
+ * CC-BY-NC-4.0
+ * See <https://creativecommons.org/licenses/by-nc-nd/4.0/> for the full license text.
  */
 
 module.exports.Webhook = require('./src/dis-logs').Webhook;
